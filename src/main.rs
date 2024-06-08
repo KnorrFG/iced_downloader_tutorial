42ac31848a92cf61aff250e68c531df01c9382d1
Felix Knorr <kontakt@felix-knorr.net>
2024-06-08 08:39:51 +0200

    enables text edit and button

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,43 @@ use iced::{
     Length, Padding, Sandbox, Settings,
 };
 
-struct Downloader;
+struct Downloader {
+    link_edit_contents: String,
+}
 
 #[derive(Debug, Clone)]
-enum Message {}
+enum Message {
+    NewDLButtonPressed,
+    LinkEditChanged(String),
+}
 
 impl Sandbox for Downloader {
     type Message = Message;
 
     fn new() -> Self {
-        Downloader
+        Downloader {
+            link_edit_contents: "".into(),
+        }
     }
 
     fn title(&self) -> String {
         "Iced Downloader Tutorial".into()
     }
 
-    fn update(&mut self, _message: Self::Message) {}
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::NewDLButtonPressed => todo!(),
+            Message::LinkEditChanged(s) => self.link_edit_contents = s,
+        }
+    }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
         container(
             row!(
-                text_input("link", ""),
+                text_input("link", &self.link_edit_contents).on_input(Message::LinkEditChanged),
                 button(text("+").horizontal_alignment(Horizontal::Center))
                     .width(Length::Fixed(40.0))
+                    .on_press(Message::NewDLButtonPressed)
             )
             .spacing(20),
         )
